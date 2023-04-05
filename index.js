ceefdfbba809a751591b1d78869b7b3b05219844
saiderrahmani <didi.mousedidi@outlook.com>
2023-04-05 00:58:04 -0400

    complete lab

--- index.js
+++ index.js
@@ -1,2 +1,33 @@
 
 // Write your code here
+ // Define num1 and num2 variables
+var num1 = 31;
+var num2 = 2;
+
+// Create multiply variable that multiplies num1 and num2
+var multiply = num1 * num2;
+
+// Check if multiply is equal to 62
+if (multiply === 62) {
+  console.log("Multiply is equal to 62.");
+} else {
+  console.log("Multiply is not equal to 62.");
+}
+// Generate a random integer greater than 0
+var random = Math.floor(Math.random() * (Number.MAX_VALUE - 1)) + 1;
+
+// Print the generated random integer
+console.log(random);
+// Define the values of num3 and num4
+var num3 = 20;
+var num4 = 8;
+
+// Calculate the remainder of num3 divided by num4, where the remainder is 4
+var mod = (num3 % num4 + num4) % num4; 
+
+// Print the value of mod
+console.log(mod); // Output: 4
+const numbers = [10, 5, 20, 15, 7]; // Example set of numbers
+const max = Math.max(...numbers); // Using Math.max() to find the highest number
+console.log(max); // Output: 20
+
